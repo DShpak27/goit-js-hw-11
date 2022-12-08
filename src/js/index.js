d64905c5b468ec5a432407d988a850cf229b6a47
DShpak27 <mr.shpak@gmail.com>
2022-12-08 10:35:00 +0200

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import '../css/styles.css';
+// import { refs } from './refs.js';
+// import { fetchCountries } from './fetchCountries.js';
+// import debounce from 'lodash.debounce';
+// import makeCountriesCard from '../templates/countries-card.hbs';
+// import makeCountryCard from '../templates/country-card.hbs';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+// export const refs = {
+//   countrySearchInput: document.querySelector('#search-box'),
+//   countryListUl: document.querySelector('.country-list'),
+//   countryInfoDiv: document.querySelector('.country-info'),
+// };
+
+// console.log(refs);
